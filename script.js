a641dc967ce0f66fc10017a22b364e61b6440aa7
Carrie Bradshaw <jeaninewilson2020@gmail.com>
2020-07-15 10:39:13 +0100

    output now stays on the page, removed auto refresh

--- script.js
+++ script.js
@@ -13,18 +13,35 @@ function getKelvin (input) {
 }
 
  function calculateTemp() {
-    getKelvin(document.getElementById('inputDegrees').value); 
-}
-
-/*
-function validateForm() {
-    console.log(document.getElementById('time'));
-
-     if (!validateDate(document.getElementById('date'))) {
-        alert("Please enter a valid date");
-        return false; 
+    // Calls the function above with the value from inputDegrees as its value 
+    const outputKelvin = getKelvin(document.getElementById('inputDegrees').value);
+    //Gets the output but doesn't stay on the DOM 
+    sessionStorage.setItem("outputElement", outputKelvin); 
+    document.getElementById('output').innerHTML = sessionStorage.getItem("outputElement"); 
+    
+    const form = document.getElementById("form"); 
+    function handleForm(event) {
+       event.preventDefault(); 
     }
-}
+    form.addEventListener('submit', handleForm); 
+    
+    /*
+    //Get the text in the field we're going to track 
+    const finalOutput = document.getElementById('output'); 
+    //See if we have an autosave value (this will happen only if the page is accidentally refreshed) 
+    if (sessionStorage.getItem("autosave")) {
+       //Restore the contents of the text field 
+      finalOutput.value = sessionStorage.getItem("autosave"); 
+   }
+   //Listen for changes in the text field 
+   finalOutput.addEventListener("change", function() {
+      // And save the results into the session storage object 
+       sessionStorage.setItem("autosave", finalOutput.value); 
+      
+   }); 
 */
-//calculateTemp(15); 
+
+}
+
+
 
